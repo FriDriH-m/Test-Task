2148835482c8c6730f373a55e6f6b8c013849ebc
Муса <good.24165@gmail.com>
2026-01-29 20:21:15 +0300

    Created Compress/Decompress methods
    Add StringCompressor.Compress() and Decompress()

--- TestTask/FirstTask.cs
+++ TestTask/FirstTask.cs
@@ -1,23 +1,66 @@
-﻿using System;
+﻿
+
 using System.Text;
 
 namespace FirstTask
 {
-    public class MyClass
+    internal class StringCompressor
     {
-        public void MyMethod()
+        public string Compress(string str)
         {
-            Console.WriteLine("MyClass MyMethod invoked.");
+            if (string.IsNullOrEmpty(str)) return str;
+
+            StringBuilder result = new StringBuilder();
+            int index = 0;
+
+            while (index < str.Length)
+            {
+                char currentChar = str[index];
+                int count = 1;
+
+                while (index + count < str.Length && str[index + count] == currentChar)
+                {
+                    count++;
+                }
+
+                result.Append(currentChar);
+
+                if (count > 1)
+                {
+                    result.Append(count);
+                }
+
+                index += count;
+            }
+
+            return result.ToString();
         }
-    }
-    class FirstTask
-    {
-        static void Main(string[] args)
+        public string Decompress(string str)
         {
-            string str = "aaAbbcCCc";           
+            if (string.IsNullOrEmpty(str)) return str;
 
+            StringBuilder result = new StringBuilder();
+            int index = 0;
 
+            while (index < str.Length)
+            {
+                char currentChar = str[index];
+                index++;
+
+                int count = 0;
+                while (index < str.Length && char.IsDigit(str[index]))
+                {
+                    count = count * 10 + (str[index] - '0'); // можно использовать int.Parse(), это понятнее, но менее эффективно
+                    index++;
+                }
+
+                if (count == 0)
+                    count = 1;
+
+                result.Append(currentChar, count);
+            }
+
+            return result.ToString();
         }
     }
 }
-
---
+++ TestTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using FirstTask;
+
+namespace Program
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            StringCompressor firstTask = new StringCompressor();
+
+            Console.WriteLine(firstTask.Compress("aaabbbbbcdaa"));
+            Console.WriteLine(firstTask.Decompress(firstTask.Compress("aaabbbbbcdaa")));
+        }
+    }
+}
+
