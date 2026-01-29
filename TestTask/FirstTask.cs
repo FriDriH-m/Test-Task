25154fe946b214a841ea44b52572121b3e6c83bf
Муса <good.24165@gmail.com>
2026-01-29 19:35:04 +0300

    Initial commit

---
+++ TestTask/FirstTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace FirstTask
+{
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("MyClass MyMethod invoked.");
+        }
+    }
+    class FirstTask
+    {
+        static void Main(string[] args)
+        {
+            string str = "aaAbbcCCc";           
+
+
+        }
+    }
+}
+
